bf7b73ae43e18a2d894066bf80ee81d02c1f7cd4
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2023-08-31 11:06:11 +0700

    config v2.1 ipaddress

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const router = require('./routes/index.js')
 const app = express()
-const port = 80
+const port = 3001
 const http = require('http')
 const path = require('path');
 const https = require('httpolyglot')
@@ -31,8 +31,8 @@ const webRtcTransport_options = {
             // ip: '192.168.205.229',
             // ip: '192.168.18.68',
             // ip: '203.194.113.166'
-            // ip: '203.194.113.166',
-            ip: '203.175.10.29'
+            ip: '203.194.113.166',
+            // ip: '203.175.10.29'
             // announcedIp: "88.12.10.41"
         }
     ],
